1de06b19ccdacc27e90ed2af717f0153024e2b2a
Philip Kerschner <pkerschner@zoominternet.net>
2022-04-06 14:48:29 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { E404Component } from './core/e404/e404.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    E404Component,
+    MenuComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Menu } from './menu.class';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  menus: Menu[] = [
+    new Menu("Home", "/home"),
+    new Menu("About", "/about"),
+    new Menu("Users", "/user/list"),
+    new Menu("Vendors", "/vendor/list"),
+    new Menu("Products", "/product/list"),
+    new Menu("Requests", "/request/list"),
+    new Menu("Login", "/login")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
